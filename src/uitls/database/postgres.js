4e8758051e369d8d7ba4ad54ab145ff8b44cde7b
Duwerney <duwerney.hernandez@zibordesarrolladores.com>
2024-10-10 16:32:23 -0500

    Changes in host

--- src/uitls/database/postgres.js
+++ src/uitls/database/postgres.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 
 const pool = new Pool({
-    host: '34.173.13.198',
+    host: '/cloudsql/my-nodejs-app-438001/my-nodejs-app-438001:us-central1:tecni-test',
     user: 'postgres',
     password: '12345',
     database: 'tecniTest',
